467aafa304ef664bcae1b346d9feec5a83e79ac2
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2019-08-26 16:56:28 +0300

    Add new proofs for step_I_to_O in zcb

--- proofs/zero_coupon_bond.v
+++ proofs/zero_coupon_bond.v
@@ -198,7 +198,7 @@ Proof.
 Qed.
 
 
-Lemma zcb_step_I_to_O_new_ctr :
+Lemma zcb_step_I_to_O :
   forall s1 s2 ctr_id dsc_id now period I O sc ctr,
     step s1 s2 ->
     consistent_state s1 ->
@@ -206,13 +206,12 @@ Lemma zcb_step_I_to_O_new_ctr :
     In ctr (m_contracts s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
-    (exists time tr,
+    (exists tr,
       In tr (m_ledger s2) /\
       tr_ctr_id tr = ctr_id /\
       tr_from tr = I /\
       tr_to tr = O /\
-      tr_amount tr = sc * 11 /\
-      tr_timestamp tr = time) \/
+      tr_amount tr = sc * 11) \/
     exists ctr,
       In ctr (m_contracts s2) /\
       ctr_primitive ctr = (At (now + period) (Scale 11 (One USD))) /\
@@ -221,36 +220,39 @@ Lemma zcb_step_I_to_O_new_ctr :
       ctr_scale ctr = sc.
 Proof.
   intros.
-  assert (C : ~ In (Executed (ctr_id ctr)) (m_events s1)).
-  apply consistent_impl_exec; auto.
-  rewrite H1 in C. simpl in C.
   induction H; subst s2.
   - simpl in *. destruct H3 as [H3 | H3]; try inversion H3.
-    contradiction.
-  - simpl in *. unfold exec_ctr_in_state_with_owner in H10.
-    unfold can_join in H5.
-    destruct H5 as [H5 | H5]; intros.
-    case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0 ctr.
-    apply zcb_execute_I_to_O_new_ctr in H10.
-    simpl in *.
-    destruct H10 as
-        [[tr (A1 & A2 & A3 & A4 & A5 & A6)] |
-         [ctr (A1 & A2 & A3 & A4 & A5 )]].
-    + subst. left. exists (m_global_time s1), tr.
-      repeat split; trivial.
-    + subst. right. exists ctr.
-      repeat split; trivial.
-      apply in_app_iff. right. trivial.
-    + case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-      subst ctr0 ctr.
-      simpl in H5. subst O. contradiction.
+    apply consistent_impl_exec in H2; auto.
+    subst ctr. simpl in *. contradiction.
   - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0 ctr. unfold zcb_desc in *. simpl in *. inversion H7.
+    subst ctr0. simpl in *. destruct H3 as [H3 | H3].
+    + unfold exec_ctr_in_state_with_owner in H10.
+      rewrite H1 in H10. simpl in H10.
+      case_analysis H10.
+      case_analysis H14.
+      * left. eexists. split. simpl. left. trivial.
+        simpl. repeat split; trivial.
+        unfold can_join in H5.
+        destruct H5 as [H5 | H5]; subst; simpl in *; trivial.
+        subst O. contradiction.
+      * right. eexists. split. rewrite in_app_iff.
+        right. simpl. left. trivial.
+        simpl. repeat split; trivial.
+        unfold can_join in H5.
+        destruct H5 as [H5 | H5]; subst; simpl in *; trivial.
+        subst O. contradiction.
+    + subst ctr. simpl in *. contradiction.
   - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0 ctr. unfold zcb_desc in *. simpl in *. inversion H7.
-  - simpl in *. destruct H3 as [H3 | H3]. inversion H3. contradiction.
-  - simpl in *. contradiction.
+    subst ctr0 ctr. unfold zcb_desc in *. simpl in H7. inversion H7.
+  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
+    subst ctr0 ctr. unfold zcb_desc in *. simpl in H7. inversion H7.
+  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
+    subst ctr0. simpl in *. destruct H3 as [H3 | H3]; try inversion H3.
+    apply consistent_impl_exec in H2; auto.
+    subst ctr. simpl in *. contradiction.
+  - subst. simpl in *.    
+    apply consistent_impl_exec in H2; auto.
+    simpl in *. contradiction.
 Qed.
 
 
@@ -286,7 +288,7 @@ Proof.
     apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
     destruct H as [H|[H|H]]; trivial.
     + eapply zcb_step_I_to_O_new_ctr in H5; eauto.
-      destruct H5 as [(t & tr & A1 & A2 & A3 & A4 & A5 & A6) | H5]; auto.
+      destruct H5 as [(t & tr & A1 & A2 & A3 & A4) | H5]; auto.
       left. exists tr. repeat split; trivial.
     + rewrite H1 in H. simpl in H.
       apply IHsteps in H.
