37aa1f799c1101e7f9c0af62e0c53355ead3f8fe
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2019-08-29 10:08:06 +0300

    Update frce proofs by using new tactics

--- proofs/fixed_rate_currency_exchange.v
+++ proofs/fixed_rate_currency_exchange.v
@@ -57,7 +57,6 @@ Lemma frce_step_I_to_O:
     consistent_state s1 ->
     ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
     In ctr (m_contracts s1) ->
-    ~ In (Executed ctr_id) (m_events s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
     exists tr,
@@ -68,41 +67,28 @@ Lemma frce_step_I_to_O:
       tr_amount tr = sc * 10 /\
       tr_timestamp tr = (m_global_time s1).
 Proof.
-  intros *.
-  intros H.
-  assert (H' := H).
-  induction H; intros.
-  - subst s2; simpl in *. destruct H9 as [H9 | H9]; try inversion H9; try contradiction.
-  - apply step_executes_only_one_contract with
-        (ctr := ctr) (ctr' := ctr0) in H'; auto.
-    + subst s2. simpl.
-      unfold can_join in H0.
-      destruct H0 as [H0 | H0]; try contradiction.
-      * subst owner ctr0.
-        rewrite H8 in H5.
-        unfold exec_ctr_in_state_with_owner in H5.
-        simpl in H5.
-        inversion H5.
-        eexists. simpl.
-        split.
-        ** left. trivial.
-        ** repeat split; trivial.
-      * subst ctr0 ctr. simpl in H0. rewrite H0 in H12. contradiction.
-    + subst ctr. simpl. trivial.
-    + subst s2. simpl. left. trivial.
-  - apply step_executes_only_one_contract with
-        (ctr := ctr) (ctr' := ctr0) in H'; auto.
-    + subst ctr ctr0. unfold frce_desc in H2. simpl in H2. inversion H2.
-    + subst ctr. simpl. trivial.
-    + subst s2. simpl. left. trivial.
-  - apply step_executes_only_one_contract with
-        (ctr := ctr) (ctr' := ctr0) in H'; auto.
-    + subst ctr ctr0. unfold frce_desc in H2. simpl in H2. inversion H2.
-    + subst ctr. simpl. trivial.
-    + subst s2. simpl. left. trivial.
-  - subst s2. simpl in *.
-    destruct H9 as [H9 | H9]; try contradiction; try inversion H9.
-  - subst s2. simpl in *. contradiction.
+  intros.
+  induction H.
+  - subst s2; simpl in *. destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H2.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr s2. simpl in *.
+    destruct H3 as [H3 | H3]; try contradiction.
+    unfold exec_ctr_in_state_with_owner in H10.
+    simpl in H10. inversion H10. subst.
+    eexists. split.
+    + simpl. left. trivial.
+    + repeat split; trivial.
+      resolve_owner H5.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr. simpl in H7. unfold frce_desc in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr. simpl in H7. unfold frce_desc in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr s2. simpl in H3.
+    destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H2.
+  - subst s2. simpl in *. find_contradiction H2.
 Qed.
 
 
@@ -112,7 +98,6 @@ Lemma frce_step_O_to_I:
     consistent_state s1 ->
     ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
     In ctr (m_contracts s1) ->
-    ~ In (Executed ctr_id) (m_events s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
     exists tr,
@@ -123,41 +108,28 @@ Lemma frce_step_O_to_I:
       tr_amount tr = sc * 11 /\
       tr_timestamp tr = (m_global_time s1).
 Proof.
-  intros *.
-  intros H.
-  assert (H' := H).
-  induction H; intros.
-  - subst s2; simpl in *. destruct H9 as [H9 | H9]; try inversion H9; try contradiction.
-  - apply step_executes_only_one_contract with
-        (ctr := ctr) (ctr' := ctr0) in H'; auto.
-    + subst s2. simpl.
-      unfold can_join in H0.
-      destruct H0 as [H0 | H0]; try contradiction.
-      * subst owner ctr0.
-        rewrite H8 in H5.
-        unfold exec_ctr_in_state_with_owner in H5.
-        simpl in H5.
-        inversion H5.
-        eexists. simpl.
-        split.
-        ** right. left. trivial.
-        ** repeat split; trivial.
-      * subst ctr0 ctr. simpl in H0. rewrite H0 in H12. contradiction.
-    + subst ctr. simpl. trivial.
-    + subst s2. simpl. left. trivial.
-  - apply step_executes_only_one_contract with
-        (ctr := ctr) (ctr' := ctr0) in H'; auto.
-    + subst ctr ctr0. unfold frce_desc in H2. simpl in H2. inversion H2.
-    + subst ctr. simpl. trivial.
-    + subst s2. simpl. left. trivial.
-  - apply step_executes_only_one_contract with
-        (ctr := ctr) (ctr' := ctr0) in H'; auto.
-    + subst ctr ctr0. unfold frce_desc in H2. simpl in H2. inversion H2.
-    + subst ctr. simpl. trivial.
-    + subst s2. simpl. left. trivial.
-  - subst s2. simpl in *.
-    destruct H9 as [H9 | H9]; try contradiction; try inversion H9.
-  - subst s2. simpl in *. contradiction.
+  intros.
+  induction H.
+  - subst s2; simpl in *. destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H2.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr s2. simpl in *.
+    destruct H3 as [H3 | H3]; try contradiction.
+    unfold exec_ctr_in_state_with_owner in H10.
+    simpl in H10. inversion H10. subst.
+    eexists. split.
+    + simpl. right. left. trivial.
+    + repeat split; trivial.
+      resolve_owner H5.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr. simpl in H7. unfold frce_desc in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr. simpl in H7. unfold frce_desc in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr s2. simpl in H3.
+    destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H2.
+  - subst s2. simpl in *. find_contradiction H2.
 Qed.
 
 
@@ -179,26 +151,17 @@ Lemma frce_steps_I_to_O:
       tr_timestamp tr = time.
 Proof.
   intros.
-  induction H; intros.
-  - subst. contradiction.
+  induction H.
+  - subst. find_contradiction H2.
   - assert (H' := H).
     apply steps_preserves_consistent_state in H'; auto.
     apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
     destruct H as [H|[H|H]]; trivial.
-    + eexists.
-      eapply frce_step_I_to_O; eauto.
-      destruct H' as [_ [_ [H' [T P]]]].
-      apply T in H.
-      destruct H as [H _].
-      subst ctr.
-      simpl in *.
-      trivial.
-    + subst ctr.
-      simpl in *.
+    + eexists. eapply frce_step_I_to_O; eauto.
+    + subst ctr. simpl in *.
       apply IHsteps in H.
       destruct H as [time [tr [S2 H]]].
-      exists time, tr.
-      split; trivial.
+      exists time, tr. split; trivial.
       eapply step_does_not_remove_transactions; eauto.
     + eapply step_does_not_remove_events in H; eauto.
       eapply step_preserves_consistent_state in H'; eauto.
@@ -215,7 +178,6 @@ Lemma frce_steps_O_to_I:
     consistent_state s1 ->
     ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
     In ctr (m_contracts s1) ->
-    ~ In (Executed ctr_id) (m_events s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
     exists time tr,
@@ -227,26 +189,17 @@ Lemma frce_steps_O_to_I:
       tr_timestamp tr = time.
 Proof.
   intros.
-  induction H; intros.
-  - subst. contradiction.
+  induction H.
+  - subst. find_contradiction H2.
   - assert (H' := H).
     apply steps_preserves_consistent_state in H'; auto.
     apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
     destruct H as [H|[H|H]]; trivial.
-    + eexists.
-      eapply frce_step_O_to_I; eauto.
-      destruct H' as [_ [_ [H' [T P]]]].
-      apply T in H.
-      destruct H as [H _].
-      subst ctr.
-      simpl in *.
-      trivial.
-    + subst ctr.
-      simpl in *.
+    + eexists. eapply frce_step_O_to_I; eauto.
+    + subst ctr. simpl in *.
       apply IHsteps in H.
       destruct H as [time [tr [S2 H]]].
-      exists time, tr.
-      split; trivial.
+      exists time, tr. split; trivial.
       eapply step_does_not_remove_transactions; eauto.
     + eapply step_does_not_remove_events in H; eauto.
       eapply step_preserves_consistent_state in H'; eauto.
