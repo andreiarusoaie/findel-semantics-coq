20164aaf1d98f8330d2c5113fca06b991bd8dffe
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2019-08-29 13:37:02 +0300

    Prove that external gateway failure does not affect the ledger

--- proofs/external_rate_currency_exchange.v
+++ proofs/external_rate_currency_exchange.v
@@ -1,18 +1,18 @@
 Load metaproperties.
 
-(* ECX : External currency exchange *)
-Definition ecx_desc (addr : Address) (sc : nat) :=
+(* ERCE : External currency exchange *)
+Definition erce_desc (addr : Address) (sc : nat) :=
   (And
      (Give (Scale sc (One EUR)))
      (ScaleObs addr (Scale sc (One USD)))
   ).
 
 (* The issuer pays the owner *)
-Lemma ecx_step_I_to_O:
+Lemma erce_step_I_to_O:
   forall s1 s2 ctr_id dsc_id addr sc amount I O ctr,
     step s1 s2 ->
     consistent_state s1 ->
-    ctr = finctr ctr_id dsc_id (ecx_desc addr amount) I O O sc ->
+    ctr = finctr ctr_id dsc_id (erce_desc addr amount) I O O sc ->
     In ctr (m_contracts s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
@@ -26,40 +26,34 @@ Proof.
   intros.
   induction H.
   - subst s2. simpl in H3. destruct H3 as [H3 | H3]; try inversion H3.
-    apply consistent_impl_exec with (ctr := ctr) in H0; auto.
-    subst ctr. contradiction.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
+    find_contradiction H0.
+  - ctr_case_analysis ctr ctr0.
     unfold exec_ctr_in_state_with_owner in H10.
-    subst ctr0 ctr. simpl in H10.
+    subst ctr. simpl in H10.
     case_match H10. destruct r. inversion H14. clear H14. subst.
     case_eq (query (m_gateway s1) addr (m_global_time s1)); intros;
       rewrite H10 in H1; inversion H1.
     inversion H1. clear H1. simpl.
     eexists. eexists. split. left. trivial.
     simpl. repeat split; eauto.
-    destruct H5 as [H5 | H5]; simpl; subst; simpl; trivial. contradiction.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0. rewrite H1 in H7. unfold ecx_desc in H7.
-    simpl in H7. inversion H7.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0. rewrite H1 in H7. unfold ecx_desc in H7.
-    simpl in H7. inversion H7.
+    resolve_owner H5.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
   - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
     subst ctr0 s2. simpl in H3.
     destruct H3 as [H3 | H3]; try inversion H3.
-    apply consistent_impl_exec with (ctr := ctr) in H0; auto.
-    subst ctr. contradiction.
-  - subst s2. simpl in *.
-    apply consistent_impl_exec with (ctr := ctr) in H0; auto.
-    subst ctr. contradiction.
+    find_contradiction H0.
+  - subst s2. simpl in *. find_contradiction H0.
 Qed.
 
 
-Lemma ecx_steps_I_to_O:
+Lemma erce_steps_I_to_O:
   forall s1 s2 ctr_id dsc_id addr sc amount I O ctr,
     steps s1 s2 ->
     consistent_state s1 ->
-    ctr = finctr ctr_id dsc_id (ecx_desc addr amount) I O O sc ->
+    ctr = finctr ctr_id dsc_id (erce_desc addr amount) I O O sc ->
     In ctr (m_contracts s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
@@ -72,13 +66,12 @@ Lemma ecx_steps_I_to_O:
 Proof.
   intros.
   induction H.
-  - subst. eapply consistent_impl_exec in H0; eauto.
-    simpl in *. contradiction.
+  - subst. find_contradiction H0.
   - assert (H' := H).
     apply steps_preserves_consistent_state in H'; auto.
     apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
     destruct H as [H | [H | H]]; trivial.
-    + eapply ecx_step_I_to_O; eauto.
+    + eapply erce_step_I_to_O; eauto.
     + subst ctr. simpl in *. eapply IHsteps in H.
       destruct H as [obs [tr [H H'']]].
       exists obs, tr. split; trivial.
@@ -93,11 +86,11 @@ Qed.
 
 
 (* The owner pays the issuer *)
-Lemma ecx_step_O_to_I:
+Lemma erce_step_O_to_I:
   forall s1 s2 ctr_id dsc_id addr sc amount I O ctr,
     step s1 s2 ->
     consistent_state s1 ->
-    ctr = finctr ctr_id dsc_id (ecx_desc addr amount) I O O sc ->
+    ctr = finctr ctr_id dsc_id (erce_desc addr amount) I O O sc ->
     In ctr (m_contracts s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
@@ -111,40 +104,34 @@ Proof.
   intros.
   induction H.
   - subst s2. simpl in H3. destruct H3 as [H3 | H3]; try inversion H3.
-    apply consistent_impl_exec with (ctr := ctr) in H0; auto.
-    subst ctr. contradiction.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
+    find_contradiction H0.
+  - ctr_case_analysis ctr ctr0.
     unfold exec_ctr_in_state_with_owner in H10.
-    subst ctr0 ctr. simpl in H10.
+    subst ctr. simpl in H10.
     case_match H10. destruct r. inversion H14. clear H14. subst.
     case_eq (query (m_gateway s1) addr (m_global_time s1)); intros;
       rewrite H10 in H1; inversion H1.
     inversion H1. clear H1. simpl.
     eexists. split. right. left. trivial.
     simpl. repeat split; eauto.
-    destruct H5 as [H5 | H5]; simpl; subst; simpl; trivial. contradiction.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0. rewrite H1 in H7. unfold ecx_desc in H7.
-    simpl in H7. inversion H7.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0. rewrite H1 in H7. unfold ecx_desc in H7.
-    simpl in H7. inversion H7.
-  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
-    subst ctr0 s2. simpl in H3.
+    resolve_owner H5.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0.
+    subst s2. simpl in H3.
     destruct H3 as [H3 | H3]; try inversion H3.
-    apply consistent_impl_exec with (ctr := ctr) in H0; auto.
-    subst ctr. contradiction.
-  - subst s2. simpl in *.
-    apply consistent_impl_exec with (ctr := ctr) in H0; auto.
-    subst ctr. contradiction.
+    find_contradiction H0.
+  - subst s2. simpl in *. find_contradiction H0.
 Qed.
 
 
-Lemma ecx_steps_O_to_I:
+Lemma erce_steps_O_to_I:
   forall s1 s2 ctr_id dsc_id addr sc amount I O ctr,
     steps s1 s2 ->
     consistent_state s1 ->
-    ctr = finctr ctr_id dsc_id (ecx_desc addr amount) I O O sc ->
+    ctr = finctr ctr_id dsc_id (erce_desc addr amount) I O O sc ->
     In ctr (m_contracts s1) ->
     In (Executed ctr_id) (m_events s2) ->
     O <> 0 ->
@@ -157,13 +144,12 @@ Lemma ecx_steps_O_to_I:
 Proof.
   intros.
   induction H.
-  - subst. eapply consistent_impl_exec in H0; eauto.
-    simpl in *. contradiction.
+  - subst. find_contradiction H0.
   - assert (H' := H).
     apply steps_preserves_consistent_state in H'; auto.
     apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
     destruct H as [H | [H | H]]; trivial.
-    + eapply ecx_step_O_to_I; eauto.
+    + eapply erce_step_O_to_I; eauto.
     + subst ctr. simpl in *. eapply IHsteps in H.
       destruct H as [tr [H H'']].
       exists tr. split; trivial.
@@ -173,4 +159,60 @@ Proof.
       exfalso. eapply H'. subst ctr. simpl in *.
       split; eauto.
       eapply step_does_not_remove_events; eauto.
+Qed.
+
+
+(* Invalid gateway section *)
+
+(* Invalid gateway implies no changes in the ledger *)
+Theorem erce_invalid_gateway:
+  forall s1 s2 ctr_id dsc_id addr sc amount I O ctr,
+    step s1 s2 ->
+    consistent_state s1 ->
+    ctr = finctr ctr_id dsc_id (erce_desc addr amount) I O O sc ->
+    In ctr (m_contracts s1) ->
+    O <> 0 ->
+    query (m_gateway s1) addr (m_global_time s1) = None ->
+    (m_ledger s1) = (m_ledger s2).
+Proof.
+  intros.
+  induction H; try subst s2; trivial; simpl in *.
+  - ctr_case_analysis ctr ctr0.
+    unfold exec_ctr_in_state_with_owner, erce_desc in H10.
+    rewrite H1 in H10. simpl in H10.
+    case_match H10. destruct r.
+    case_match H12. rewrite H4 in H10. inversion H10.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+Qed.
+
+
+Lemma erce_invalid_gtw_step:
+  forall s1 s2 ctr_id dsc_id addr sc amount I O ctr,
+    step s1 s2 ->
+    consistent_state s1 ->
+    ctr = finctr ctr_id dsc_id (erce_desc addr amount) I O O sc ->
+    In ctr (m_contracts s1) ->
+    In (Deleted ctr_id) (m_events s2) ->
+    O <> 0 ->
+    (m_ledger s1) = (m_ledger s2).
+Proof.
+  intros.
+  induction H.
+  - subst s2. simpl in H3. destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H0.
+  - case_eq (ctr_eq_dec ctr ctr0); intros; try contradiction.
+    subst ctr0 s2. simpl in H3.
+    destruct H3 as [H3 | H3]; try inversion H3.
+    destruct H0 as [_ [_ [_ [H0 _]]]].
+    apply H0 in H2. destruct H2 as [_ H2].
+    subst ctr. simpl in *. try contradiction.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0. rewrite H1 in H7.
+    unfold erce_desc in H7. simpl in H7. inversion H7.
+  - ctr_case_analysis ctr ctr0. subst s2. simpl. trivial.
+  - subst s2. simpl in *. find_contradiction H0.
 Qed.
