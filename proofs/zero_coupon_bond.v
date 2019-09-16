5e7487264566966e092adbfb1a8f01466f500dcb
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2019-09-16 17:56:09 +0300

    add new proof style for zcb

--- proofs/zero_coupon_bond.v
+++ proofs/zero_coupon_bond.v
@@ -9,7 +9,6 @@ Definition zcb_desc
      (At (NOW + PERIOD) (Scale 11 (One USD)))
   ).
 
-
 (* The issuer's rights *)
 Lemma zcb_execute_O_to_I :
   forall sc now period I O bal time gtw ctr_id dsc_id next_id ledger result,
@@ -83,7 +82,7 @@ Qed.
 
 
 (* If the owner joins, the issuer receives sc * 10 USD from the owner *)
-Theorem zcb_steps_O_to_I :
+Proposition zcb_steps_O_to_I :
   forall s1 s2 ctr_id dsc_id now period I O sc ctr,
     steps s1 s2 ->
     consistent_state s1 ->
@@ -128,6 +127,192 @@ Print zcb_steps_O_to_I.
 1. If  current time < now + period -> the owner can trigger payment later.
 2. If current time  = now + period and the owner triggers -> the owner gets paid.
 *)
+
+Lemma zcb_step_I_to_O :
+  forall s1 s2 ctr_id dsc_id now period I O sc ctr,
+    step s1 s2 ->
+    consistent_state s1 ->
+    ctr = finctr ctr_id dsc_id (zcb_desc now period) I O O sc ->
+    In ctr (m_contracts s1) ->
+    In (Executed ctr_id) (m_events s2) ->
+    O <> 0 ->
+    (m_global_time s1) < now + period - Δ ->
+    exists ctr,
+      In ctr (m_contracts s2) /\
+      ctr_primitive ctr = (At (now + period) (Scale 11 (One USD))) /\
+      ctr_issuer ctr = I /\
+      ctr_proposed_owner ctr = O /\
+      ctr_scale ctr = sc.
+Proof.
+  intros.
+  induction H.
+  - subst s2. find_contradiction H2.
+    destruct H3 as [H3 | H3]; try inversion H3; try contradiction.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr s2. simpl in *.
+    destruct H3 as [H3 | H3]; try contradiction.
+    + unfold exec_ctr_in_state_with_owner in *.
+      simpl in H11. inversion H11.
+      case_analysis H12.
+      case_analysis H15.
+      * apply ltb_sound_true in H12.
+        apply ltb_sound_false in H1.
+        omega.
+      * eexists. split; simpl.
+        ** rewrite in_app_iff. right. simpl. left. eauto.
+        ** repeat split; trivial. resolve_owner H6.
+  - ctr_case_analysis ctr ctr0. subst ctr s2. simpl in H8.
+    unfold zcb_desc in H8. inversion H8.
+  - ctr_case_analysis ctr ctr0. subst ctr s2. simpl in H8.
+    unfold zcb_desc in H8. inversion H8.
+  - ctr_case_analysis ctr ctr0. subst ctr s2.
+    simpl in H3.
+    destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H2.
+  - subst. simpl in *. find_contradiction H2.
+Qed.
+
+Lemma zcb_step_I_to_O' :
+  forall s1 s2 ctr_id dsc_id now period I O sc ctr,
+    step s1 s2 ->
+    consistent_state s1 ->
+    ctr = finctr ctr_id dsc_id (zcb_desc now period) I O O sc ->
+    In ctr (m_contracts s1) ->
+    In (Executed ctr_id) (m_events s2) ->
+    O <> 0 ->
+    (m_global_time s2) < now + period - Δ ->
+    exists ctr,
+      In ctr (m_contracts s2) /\
+      ctr_primitive ctr = (At (now + period) (Scale 11 (One USD))) /\
+      ctr_issuer ctr = I /\
+      ctr_proposed_owner ctr = O /\
+      ctr_scale ctr = sc.
+Proof.
+  intros.
+  induction H.
+  - subst s2. find_contradiction H2.
+    destruct H3 as [H3 | H3]; try inversion H3; try contradiction.
+  - ctr_case_analysis ctr ctr0.
+    subst ctr s2. simpl in *.
+    destruct H3 as [H3 | H3]; try contradiction.
+    + unfold exec_ctr_in_state_with_owner in *.
+      simpl in H11. inversion H11.
+      case_analysis H12.
+      case_analysis H15.
+      * apply ltb_sound_true in H12.
+        apply ltb_sound_false in H1.
+        omega.
+      * eexists. split; simpl.
+        ** rewrite in_app_iff. right. simpl. left. eauto.
+        ** repeat split; trivial. resolve_owner H6.
+  - ctr_case_analysis ctr ctr0. subst ctr s2. simpl in H8.
+    unfold zcb_desc in H8. inversion H8.
+  - ctr_case_analysis ctr ctr0. subst ctr s2. simpl in H8.
+    unfold zcb_desc in H8. inversion H8.
+  - ctr_case_analysis ctr ctr0. subst ctr s2.
+    simpl in H3.
+    destruct H3 as [H3 | H3]; try inversion H3.
+    find_contradiction H2.
+  - subst. simpl in *. find_contradiction H2.
+Qed.
+
+
+Lemma zcb_steps_I_to_O':
+  forall s1 s2 ctr_id dsc_id now period I O sc ctr,
+    steps s1 s2 ->
+    consistent_state s1 ->
+    ctr = finctr ctr_id dsc_id (zcb_desc now period) I O O sc ->
+    In ctr (m_contracts s1) ->
+    In (Executed ctr_id) (m_events s2) ->
+    O <> 0 ->
+    (m_global_time s1) > now + period + Δ ->
+    (m_global_time s2) < now + period - Δ ->
+    exists ctr,
+      In ctr (m_contracts s2) /\
+      ctr_primitive ctr = (At (now + period) (Scale 11 (One USD))) /\
+      ctr_issuer ctr = I /\
+      ctr_proposed_owner ctr = O /\
+      ctr_scale ctr = sc.
+Proof.
+  intros.
+  induction H.
+  - subst. find_contradiction H2.
+  - assert (H' := H). assert (H'' := H).
+    apply steps_preserves_consistent_state in H'; auto.
+    apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
+    destruct H as [H|[H|H]]; trivial.
+    + eapply zcb_step_I_to_O'; eauto.
+    + assert (S := H6).
+      apply time_inc in H6.
+      assert (m_global_time s <= now + period - Δ); try omega.
+      subst ctr. simpl in *.
+      apply IHsteps in H; auto.
+      destruct H as [ctr [H [A HT]]].
+      exists ctr.
+      split; trivial.
+      ++ induction S; subst s2; simpl; auto.
+         ** ctr_case_analysis ctr ctr0.
+            destruct ctr. simpl in *.
+            rewrite A in H13.
+            clear H14 H12 H10 H9 IHsteps H11 H3 H4 H1 H8 H2 H A HT.
+            unfold exec_ctr_in_state_with_owner in H13. simpl in *.
+            case_if H13.
+            case_if H17.
+            *** apply ltb_sound_true in H1.
+                apply ltb_sound_false in H.
+                omega.
+            *** apply ltb_sound_false in H1.
+                apply ltb_sound_false in H. 
+                contradict H13. auto.
+
+            destruct (ctr_primitive ctr); try inversion A.
+            subst. simpl in H5, H6.
+
+
+        struct (ctr_primitive ctr); inversion A.
+         subst. 
+         
+
+      admit.
+    + eapply step_preserves_consistent_state in H'; eauto.
+      eapply step_does_not_remove_events in H6; eauto.
+      destruct H' as [_ [_ [_ [_ T]]]].
+      unfold not in T.
+      exfalso.
+      eapply T. subst ctr.
+      split; eauto.
+Admitted.
+
+Lemma timebound_postpone:
+  forall s s' c p t0 t1,
+    step s s' ->
+    consistent_state s ->
+    In c (m_contracts s) ->
+    m_global_time s <= t0 ->
+    m_global_time s' <= t0 ->
+    ctr_primitive c = (Timebound t0 t1 p) ->
+    exists c', In c' (m_contracts s') /\ ctr_primitive c' = p.
+Proof.
+  intros.
+  induction H; subst s'; simpl.
+  - right. trivial.
+  - ctr_case_analysis c ctr.
+    apply in_app_iff.
+    unfold exec_ctr_in_state_with_owner in H8.
+    induction (ctr_primitive c); simpl in *; try inversion H3.
+    subst. 
+    clear H6 H.
+    subst.
+    clear H10 H3.
+    
+    + apply ltb_sound_true in H9.
+      apply ltb_sound_false in H11.
+      try omega.
+    + simpl. subst.
+      clear H2 H5 H0.
+
+
+
 
 (* Failed attempt: cannot prove that the owner always gets paid! *)
 Lemma zcb_execute_I_to_O :
