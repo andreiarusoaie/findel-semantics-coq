60dce7bb4e9f0f768bfa35f0f6c4667c18c74907
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2019-09-19 12:36:17 +0300

    update frce propositions and proofs

--- proofs/fixed_rate_currency_exchange.v
+++ proofs/fixed_rate_currency_exchange.v
@@ -7,35 +7,12 @@ Definition frce_desc :=
      (Scale 10 (One EUR))
   ).
 
-
-(* The owner's rights *)
-Lemma frce_execute_I_to_O :
-  forall sc I O bal time gtw ctr_id dsc_id next_id ledger result,
-    execute frce_desc sc I O bal time gtw ctr_id dsc_id next_id ledger =
-    Some result ->
-    exists tr,
-      In tr (res_ledger result) /\
-      tr_ctr_id tr = ctr_id /\
-      tr_from tr = I /\
-      tr_to tr = O /\
-      tr_amount tr = sc * 10 /\
-      tr_currency tr = EUR.
-Proof.
-  intros. unfold frce_desc in H. simp_inv_clear H.
-  eexists. simpl.
-  split.
-  - left. trivial.
-  - repeat split; simpl; trivial.
-Qed.
-
-
-Lemma frce_step_I_to_O:
-  forall s1 s2 ctr ctr_id dsc_id I O sc,
-    step s1 s2 ->
+(* The issuer receives scale * 10 EUR from the owner (who joins) *)
+Proposition frce_I_to_O :
+  forall s1 s2 ctr_id dsc_id I O sc ctr t,
     consistent_state s1 ->
     ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
-    In ctr (m_contracts s1) ->
-    In (Executed ctr_id) (m_events s2) ->
+    joins O ctr s1 s2 t ->
     O <> 0 ->
     exists tr,
       In tr (m_ledger s2) /\
@@ -46,99 +23,40 @@ Lemma frce_step_I_to_O:
       tr_currency tr = EUR.
 Proof.
   intros.
-  induction H.
-  - subst s2; simpl in *. destruct H3 as [H3 | H3]; try inversion H3.
-    find_contradiction H2.
-  - ctr_case_analysis ctr ctr0.
-    subst ctr s2. simpl in *.
-    destruct H3 as [H3 | H3]; try contradiction.
-    unfold exec_ctr_in_state_with_owner in H10.
-    simpl in H10. inversion H10. subst.
-    eexists. split.
-    + simpl. left. trivial.
-    + repeat split; trivial.
-      resolve_owner H5.
-  - ctr_case_analysis ctr ctr0.
-    subst ctr. unfold frce_desc in H7. simp_inv_clear H7. 
-  - ctr_case_analysis ctr ctr0.
-    subst ctr. unfold frce_desc in H7. simp_inv_clear H7. 
-  - ctr_case_analysis ctr ctr0.
-    subst ctr s2. simpl in H3.
-    destruct H3 as [H3 | H3]; try inversion H3.
-    find_contradiction H2.
-  - subst s2. simpl in *. find_contradiction H2.
+  destruct_join H1.
+  - insert_consistent s Ss.
+    induction St; subst s'.
+    + inversion_event Ev. find_contradiction Ev.
+    + ctr_case_analysis ctr ctr0.
+      execute_own ctr H9.
+      inversion H9.
+      eexists. split.
+      eapply steps_does_not_remove_transactions; eauto.
+      simpl. subst ledger'. simpl. left. trivial.
+      repeat split; trivial. resolve_owner H4.
+    + not_or ctr ctr0 H6.
+    + not_or ctr ctr0 H6.
+    + ctr_case_analysis ctr ctr0. inversion_event Ev. find_contradiction Ev.
+    + find_contradiction Ev.
+  - insert_consistent s Ss.
+    induction St; subst s'.
+    + inversion_event Ev. find_contradiction_del Ev.
+    + ctr_case_analysis ctr ctr0. inversion_event Ev. find_contradiction_del Ev.
+    + not_or ctr ctr0 H6.
+    + not_or ctr ctr0 H6.
+    + ctr_case_analysis ctr ctr0. execute_own ctr H7. inversion H7.
+    + find_contradiction_del Ev.
 Qed.
 
-(* If the owner joins, then the owner receives sc * n * 10 EUR from the issuer *)
-Proposition frce_steps_I_to_O:
-  forall s1 s2 ctr ctr_id dsc_id I O sc,
-    steps s1 s2 ->
+Print frce_I_to_O.
+
+
+(* The owner receives scale * 11 USD from the issuer *)
+Proposition frce_O_to_I :
+  forall s1 s2 ctr_id dsc_id I O sc ctr t,
     consistent_state s1 ->
     ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
-    In ctr (m_contracts s1) ->
-    In (Executed ctr_id) (m_events s2) ->
-    O <> 0 ->
-    exists tr,
-      In tr (m_ledger s2) /\
-      tr_ctr_id tr = ctr_id /\
-      tr_from tr = I /\
-      tr_to tr = O /\
-      tr_amount tr = sc * 10 /\
-      tr_currency tr = EUR.
-Proof.
-  intros.
-  induction H.
-  - subst. find_contradiction H2.
-  - assert (H' := H). assert (H'' := H).
-    eapply steps_preserves_consistent_state in H'; eauto.
-    apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
-    destruct H as [H|[H|H]]; trivial.
-    + eapply frce_step_I_to_O; eauto.
-    + subst ctr. simpl in *.
-      apply IHsteps in H.
-      destruct H as [tr [S2 H]].
-      exists tr. split; trivial.
-      eapply step_does_not_remove_transactions; eauto.
-    + eapply step_does_not_remove_events in H; eauto.
-      eapply step_preserves_consistent_state in H'; eauto.
-      destruct H' as [_ [_ [H' [T P]]]].
-      subst ctr. simpl in *.
-      contradiction P with (e := ctr_id0).
-      split; trivial.
-Qed.
-
-Print frce_steps_I_to_O.
-
-
-(* The issuer's rights *)
-(* The owner's rights *)
-Lemma frce_execute_O_to_I :
-  forall sc I O bal time gtw ctr_id dsc_id next_id ledger result,
-    execute frce_desc sc I O bal time gtw ctr_id dsc_id next_id ledger =
-    Some result ->
-    exists tr,
-      In tr (res_ledger result) /\
-      tr_ctr_id tr = ctr_id /\
-      tr_from tr = O /\
-      tr_to tr = I /\
-      tr_amount tr = sc * 11 /\
-      tr_currency tr = USD.
-Proof.
-  intros. unfold frce_desc in H. simp_inv_clear H.
-  eexists. simpl.
-  split.
-  - right. left. trivial.
-  - repeat split; simpl; trivial.
-Qed.
-
-
-Lemma frce_step_O_to_I:
-  forall s1 s2 ctr ctr_id dsc_id I O sc,
-    step s1 s2 ->
-    consistent_state s1 ->
-    ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
-    In ctr (m_contracts s1) ->
-    In (Executed ctr_id) (m_events s2) ->
+    joins O ctr s1 s2 t ->
     O <> 0 ->
     exists tr,
       In tr (m_ledger s2) /\
@@ -149,65 +67,29 @@ Lemma frce_step_O_to_I:
       tr_currency tr = USD.
 Proof.
   intros.
-  induction H.
-  - subst s2; simpl in *. destruct H3 as [H3 | H3]; try inversion H3.
-    find_contradiction H2.
-  - ctr_case_analysis ctr ctr0.
-    subst ctr s2. simpl in *.
-    destruct H3 as [H3 | H3]; try contradiction.
-    unfold exec_ctr_in_state_with_owner in H10.
-    simpl in H10. inversion H10. subst.
-    eexists. split.
-    + simpl. right. left. trivial.
-    + repeat split; trivial.
-      resolve_owner H5.
-  - ctr_case_analysis ctr ctr0.
-    subst ctr. unfold frce_desc in H7. simp_inv_clear H7.
-  - ctr_case_analysis ctr ctr0.
-    subst ctr. unfold frce_desc in H7. simp_inv_clear H7.
-  - ctr_case_analysis ctr ctr0.
-    subst ctr s2. simpl in H3.
-    destruct H3 as [H3 | H3]; try inversion H3.
-    find_contradiction H2.
-  - subst s2. simpl in *. find_contradiction H2.
+  destruct_join H1.
+  - insert_consistent s Ss.
+    induction St; subst s'.
+    + inversion_event Ev. find_contradiction Ev.
+    + ctr_case_analysis ctr ctr0.
+      execute_own ctr H9.
+      inversion H9.
+      eexists. split.
+      eapply steps_does_not_remove_transactions; eauto.
+      simpl. subst ledger'. simpl. right. left. trivial.
+      repeat split; trivial. resolve_owner H4.
+    + not_or ctr ctr0 H6.
+    + not_or ctr ctr0 H6.
+    + ctr_case_analysis ctr ctr0. inversion_event Ev. find_contradiction Ev.
+    + find_contradiction Ev.
+  - insert_consistent s Ss.
+    induction St; subst s'.
+    + inversion_event Ev. find_contradiction_del Ev.
+    + ctr_case_analysis ctr ctr0. inversion_event Ev. find_contradiction_del Ev.
+    + not_or ctr ctr0 H6.
+    + not_or ctr ctr0 H6.
+    + ctr_case_analysis ctr ctr0. execute_own ctr H7. inversion H7.
+    + find_contradiction_del Ev.
 Qed.
 
-(* If the owner joins, then the issuer receives sc * n * 11 USD from the owner *)
-Proposition frce_steps_O_to_I:
-  forall s1 s2 ctr ctr_id dsc_id I O sc,
-    steps s1 s2 ->
-    consistent_state s1 ->
-    ctr = finctr ctr_id dsc_id frce_desc I O O sc ->
-    In ctr (m_contracts s1) ->
-    In (Executed ctr_id) (m_events s2) ->
-    O <> 0 ->
-    exists tr,
-      In tr (m_ledger s2) /\
-      tr_ctr_id tr = ctr_id /\
-      tr_from tr = O /\
-      tr_to tr = I /\
-      tr_amount tr = sc * 11 /\
-      tr_currency tr = USD.
-Proof.
-  intros.
-  induction H.
-  - subst. find_contradiction H2.
-  - assert (H' := H). assert (H'' := H).
-    eapply steps_preserves_consistent_state in H'; eauto.
-    apply steps_effect_over_contract with (ctr := ctr) in H; trivial.
-    destruct H as [H|[H|H]]; trivial.
-    + eapply frce_step_O_to_I; eauto.
-    + subst ctr. simpl in *.
-      apply IHsteps in H.
-      destruct H as [tr [S2 H]].
-      exists tr. split; trivial.
-      eapply step_does_not_remove_transactions; eauto.
-    + eapply step_does_not_remove_events in H; eauto.
-      eapply step_preserves_consistent_state in H'; eauto.
-      destruct H' as [_ [_ [H' [T P]]]].
-      subst ctr. simpl in *.
-      contradiction P with (e := ctr_id0).
-      split; trivial.
-Qed.
-
-Print frce_steps_O_to_I.
+Print frce_O_to_I.
