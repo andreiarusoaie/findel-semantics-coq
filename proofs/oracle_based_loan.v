82129c1226a905e83ab38ab862180c2ae78a5af7
Andrei Arusoaie <andrei.arusoaie@gmail.com>
2019-12-07 00:21:23 +0200

    prove incremental step in cds

--- proofs/oracle_based_loan.v
+++ proofs/oracle_based_loan.v
@@ -162,6 +162,50 @@ Qed.
 (*       (At time (default_payment defaulted_addr price FY YL)). *)
 
 
+Lemma ltb_complete_true:
+  forall a b,
+    a < b -> a <? b = true.
+Proof.
+  induction a; intros.
+  - destruct b. inversion H. unfold Nat.ltb. simpl. trivial.
+  - destruct b.
+    + inversion H.
+    + apply lt_S_n in H. assert (H' := H). apply IHa in H.
+      unfold Nat.ltb in *. simpl in *. destruct b; auto.
+Qed.
+
+Lemma ltb_complete_false:
+  forall a b,
+    ~ a < b -> a <? b = false.
+Proof.
+  induction a; intros.
+  - destruct b; auto. contradict H. omega.
+  - destruct b.
+    + unfold Nat.ltb. simpl. trivial.
+    + SearchPattern (_ -> S _ < S _).
+      unfold not in H.
+      assert (H' : a < b -> False).
+      intros. apply H. apply lt_n_S. assumption.
+      apply IHa in H'.
+      unfold Nat.ltb in *. simpl in *. destruct b; auto.
+Qed.
+
+Lemma helper :
+  forall b a,
+    a > b ->
+    b > 0 ->
+    a - b <? a = true.
+Proof.
+  intros. apply ltb_complete_true. omega.
+Qed.
+
+Lemma helper' : forall a b,
+    a > b ->
+    a + b <? a = false.
+Proof.
+  intros. apply ltb_complete_false. omega.
+Qed.
+
 
 (*
 If the C fails to pay the yearly fee (FY), then
@@ -177,6 +221,7 @@ Proposition yearly_check_default_true_Alice_rights :
     O <> 0 ->
     query (m_gateway s) defaulted_addr (m_global_time s) = Some response ->
     response <> 0 ->
+    time > Δ ->
     (exists tr,
       In tr (m_ledger s2) /\
       tr_ctr_id tr = ctr_id /\
@@ -190,32 +235,43 @@ Proof.
     induction St; subst s0.
     + inversion_event Ev. find_contradiction Ev.
     + ctr_case_analysis ctr ctr0.
-      execute_own ctr H11.
+      execute_own ctr H12.
       case_eq (response); intros; subst response; try contradiction.
-      rewrite H3 in H11. simpl in H11.
-      case_match H11.
-      case_if H11.
-      case_if H15.
-      * destruct r. inversion H16. subst.
+      rewrite H3 in H12. simpl in H12.
+      case_match H12.
+      case_if H12.
+      case_if H16.
+      * destruct r. inversion H17. subst.
         eexists. split.
-        ** inversion H14.
+        ** inversion H15.
            eapply steps_does_not_remove_transactions; eauto.
            simpl. subst ledger'. simpl. left. trivial.
-        ** repeat split; trivial. resolve_owner H6.
+        ** repeat split; trivial. resolve_owner H7.
            simpl. omega.
-      * 
-    + not_or ctr ctr0 H8.
-    + not_or ctr ctr0 H8.
+      * apply helper in H5; eauto. subst time. rewrite H5 in H0. inversion H0.
+        unfold Δ. omega.
+    + not_or ctr ctr0 H9.
+    + not_or ctr ctr0 H9.
     + ctr_case_analysis ctr ctr0. inversion_event Ev. find_contradiction Ev.
     + find_contradiction Ev.
   - insert_consistent s Ss.
-    induction St; subst s'.
+    induction St; subst s0.
     + inversion_event Ev. find_contradiction_del Ev.
     + ctr_case_analysis ctr ctr0. inversion_event Ev. find_contradiction_del Ev.
-    + not_or ctr ctr0 H6.
-    + not_or ctr ctr0 H6.
-    + ctr_case_analysis ctr ctr0. execute_own ctr H7. inversion H7.
+    + not_or ctr ctr0 H9.
+    + not_or ctr ctr0 H9.
+    + ctr_case_analysis ctr ctr0. execute_own ctr H10.
+      case_eq (response); intros; subst response; try contradiction.
+      rewrite H3 in H10. simpl in H10.
+      case_match H10.
+      case_if H0.
+      case_if H14.
+      * destruct r. inversion H15; clear H15; subst.
+        inversion H13.
+      * apply helper in H5; eauto. subst time. rewrite H5 in H0. inversion H0.
+        unfold Δ. omega.
+      * case_if H0.
+        ** subst. contradict H10. rewrite helper'; auto.
+        ** case_if H13.
     + find_contradiction_del Ev.
 Qed.
-
-Print frce_O_to_I.
